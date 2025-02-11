992011460e9b4c675eca59fbfc14dc3d0fcb38a8
pechikonar <muthukonaruot@gmail.com>
2025-02-10 22:43:44 -0500

    Bootstrap installed and updated main to import bootstrap

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import App from './App.tsx';
 import CandidateSearch from './pages/CandidateSearch.tsx';
 import SavedCandidates from './pages/SavedCandidates.tsx';
 import ErrorPage from './pages/ErrorPage.tsx';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const router = createBrowserRouter([
   {
